b2c4c25a234300ed3f72bbd9ebaa16194652f13e
david <david>
2026-03-29 22:32:41 +0200

    first commit

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,21 +1,30 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
-  reducers: {
-    addItem: (state, action) => {
-    
+    name: "cart",
+    initialState: {
+        items: [], // Initialize items as an empty array
     },
-    removeItem: (state, action) => {
+    reducers: {
+        addItem: (state, action) => {
+            const { name, cost, image } = action.payload;
+            const existingItem = state.items.find((item) => item.name === name);
+            existingItem
+                ? existingItem.quantity++
+                : state.items.push({ name, cost, image, quantity: 1 });
+        },
+        removeItem: (state, action) => {
+            const { name } = action.payload;
+            state.items = state.items.filter((item) => item.name != name);
+        },
+        updateQuantity: (state, action) => {
+            const { name, quantity } = action.payload;
+            const itemToUpdate = state.items.find((item) => item.name === name);
+            if (itemToUpdate) {
+                itemToUpdate.quantity = quantity - 1;
+            }
+        },
     },
-    updateQuantity: (state, action) => {
-
-    
-    },
-  },
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
